3fb601f7a51f051784482b4442f03389fb70b631
Jopdahl12 <opdajo01@luther.edu>
2014-10-28 10:37:40 -0500

    uhg

--- youTubeMash.js
+++ youTubeMash.js
@@ -32,7 +32,7 @@ function handleAuthClick(event) {
 }
 
 function makeApiCall() {
-    gapi.client.load('youtube', 'v3', search);
+    gapi.client.load('youtube', 'v3').then(search());
 }
 
 function search() {
