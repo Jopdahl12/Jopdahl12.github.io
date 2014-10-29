4f5425a2fbcfc62589effba938ba4042bc65fcb8
Jopdahl12 <opdajo01@luther.edu>
2014-10-29 17:00:17 -0500

    a

--- playVideo.js
+++ playVideo.js
@@ -1,13 +1,26 @@
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    events: {
-      'onReady': onPlayerReady,
-      //'onStateChange': onPlayerStateChange
-    }
-  });
-}
+var tag = document.createElement('script');
+    tag.src = "//www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-function onPlayerReady(event) {
-	event.target.playVideo();
-}
+    var player;
+    function onYouTubePlayerAPIReady() {
+        player = new YT.Player('player', {
+            height: '390',
+            width: '640',
+            videoId: 'u1zgFlCw8Aw',
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    }
+
+    function onPlayerReady() {
+        console.log('ready');
+    }
+
+    function onPlayerStateChange() {
+        console.log('player changed');
+    }
+});
