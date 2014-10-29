7f9835e31508bef9a7ce164f83e4c697bf864ac6
Jopdahl12 <opdajo01@luther.edu>
2014-10-29 14:34:26 -0500

    array

--- search.js
+++ search.js
@@ -12,18 +12,17 @@ function search() {
     createList(str)});
 }
 function createList(str) {
-	console.log('creating list');
 	var array= JSON.parse(str);
 	console.log(array);
 	var list= document.getElementById("videos");
 	for (i=0; i < array.length ; i++) {
 		var entry = document.createElement('li');
-		var thumbnail = array[i].snippet.thumbnails.default;
-		var title= document.createTextNode(array[i].snippet.title);
+		var thumbnail = array[items[i]].snippet.thumbnails.default;
+		var title= document.createTextNode(array[items[i]]snippet.title);
 		entry.appendChild(thumbnail);
 		entry.appendChild(title);
 		entry.setClassName= "theseVids";
-		entry.onclick=playVideo(array[i].id.videoId);
+		entry.onclick=playVideo(array[items[i]].id.videoId);
 		list.appendChild(entry);
 	}
 }
