d5958d0aa8115c8233cfb941048c12586eee906c
Jopdahl12 <opdajo01@luther.edu>
2014-10-29 14:51:36 -0500

    thumbnail

--- search.js
+++ search.js
@@ -17,9 +17,10 @@ function createList(str) {
 	var list= document.getElementById("videos");
 	for (i=0; i < 5 ; i++) {
 		var entry = document.createElement('li');
-		var thumbnail = array.items[i].snippet.thumbnails.default;
+		var thumbnail = document.createElement('img');
+		thumbnail.src= array.items[i].snippet.thumbnails.default;
 		var title= document.createTextNode(array.items[i].snippet.title);
-		//entry.appendChild(thumbnail);
+		entry.appendChild(thumbnail);
 		entry.appendChild(title);
 		entry.setClassName= "theseVids";
 		//entry.onclick=playVideo(array.items[i].id.videoId);
