3ff3b4f23e517d9668aa7bc6e840228f4673215e
Jopdahl12 <opdajo01@luther.edu>
2014-10-29 15:28:24 -0500

    try to fix id

--- search.js
+++ search.js
@@ -4,7 +4,7 @@ function search() {
   var request = gapi.client.youtube.search.list({
     type: 'video',
     q: q,
-    part: 'snippet , id' 
+    part: 'snippet' 
   });
 
   request.execute(function(response) {
