a71d84c13651d9af53f387c8d05e482a26e39a2e
Jopdahl12 <opdajo01@luther.edu>
2014-10-28 22:16:36 -0500

    a

--- search.js
+++ search.js
@@ -16,6 +16,7 @@ function handleAPILoaded() {
 function search() {
   var q = $('#query').val();
   var request = gapi.client.youtube.search.list({
+    type: 'video',
     q: q,
     part: 'snippet'
   });
@@ -23,6 +24,6 @@ function search() {
   request.execute(function(response) {
     var str = JSON.stringify(response.result);
     console.log(str);
-    $('#search-container').html('<pre>' + str + '</pre>');
+    $('#vidbar').html('<pre>' + str + '</pre>');
   });
 }
