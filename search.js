b451ce66c2694b11cec8612318a63921daf1eb50
Juan <navaju01@luther.edu>
2014-10-30 01:53:43 -0500

    fae

--- search.js
+++ search.js
@@ -43,10 +43,28 @@ function clicked() {
 function selectVid(ID) {
 	playVideo(ID);
 	console.log('clciked');
-	function createMap()
+	
 	displayVideoAnalytics(ID);
 	
 }
+
+function initialize() {
+console.log("initializing");
+    var myLatLng = new google.maps.LatLng(-34.397,150.644);
+  var mapOptions = {
+    center: myLatLng,
+    zoom: 8
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+  
+  var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title:"Hello World!"
+  });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
 
 function playVideo(ID) {
 	var source;
