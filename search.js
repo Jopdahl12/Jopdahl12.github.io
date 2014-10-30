3fe48cb4e481ac3948802827ab51992ee598d6e4
Jopdahl12 <opdajo01@luther.edu>
2014-10-29 20:10:34 -0500

    revert

--- search.js
+++ search.js
@@ -19,90 +19,23 @@ function createList(str) {
 	if ($("#videos").has("li").length != 0) {
 		clearList();
 		}
-	var entry1 = document.createElement('li');
-	var thumbnail1 = document.createElement('img');
-	thumbnail1.src= array.items[0].snippet.thumbnails.default.url;
-	var title1= document.createTextNode(array.items[0].snippet.title);
-	entry1.appendChild(thumbnail);
-	entry1.appendChild(title);
-	entry1.appendChild(array.items[0].id.videoId);
-	entry1.className = "theseVids";
-	entry1.id = '1';
-	list.appendChild(entry1);
-	
-	var entry2 = document.createElement('li');
-	var thumbnail2 = document.createElement('img');
-	thumbnail2.src= array.items[1].snippet.thumbnails.default.url;
-	var title2= document.createTextNode(array.items[1].snippet.title);
-	entry2.appendChild(thumbnail);
-	entry2.appendChild(title);
-		entry2.appendChild(array.items[1].id.videoId);
-	entry2.className = "theseVids";
-	entry2.id = '2';
-	list.appendChild(entry2);
-	
-	var entry3 = document.createElement('li');
-	var thumbnail3 = document.createElement('img');
-	thumbnail3.src= array.items[2].snippet.thumbnails.default.url;
-	var title3= document.createTextNode(array.items[2].snippet.title);
-	entry3.appendChild(thumbnail);
-	entry3.appendChild(title);
-		entry3.appendChild(array.items[2].id.videoId);
-	entry3.className = "theseVids";
-	entry3.id = '3';
-	list.appendChild(entry3);
-	
-	var entry4 = document.createElement('li');
-	var thumbnail4 = document.createElement('img');
-	thumbnail4.src= array.items[3].snippet.thumbnails.default.url;
-	var title4= document.createTextNode(array.items[3].snippet.title);
-	entry4.appendChild(thumbnail);
-	entry4.appendChild(title);
-		entry4.appendChild(array.items[3].id.videoId);
-	entry4.className = "theseVids";
-	entry4.id = '4';
-	list.appendChild(entry4);
-	
-	var entry5 = document.createElement('li');
-	var thumbnail5 = document.createElement('img');
-	thumbnail5.src= array.items[4].snippet.thumbnails.default.url;
-	var title5= document.createTextNode(array.items[4].snippet.title);
-	entry5.appendChild(thumbnail);
-	entry5.appendChild(title);
-		entry5.appendChild(array.items[4].id.videoId);
-	entry5.className = "theseVids";
-	entry5.id = '5';
-	list.appendChild(entry5);
-	
-	afterListCreated();
+	for (i=0;i<5;i++){
+		var entry = document.createElement('li');
+		var thumbnail = document.createElement('img');
+		thumbnail.src= array.items[i].snippet.thumbnails.default.url;
+		var title= document.createTextNode(array.items[i].snippet.title);
+		entry.appendChild(thumbnail);
+		entry.appendChild(title);
+		entry.appendChild(array.items[i].id.videoId);
+		entry.className = "theseVids";
+		entry.onclick = selectVid;
+		list.appendChild(entry1);
+}
 }
 
-function afterListCreated() {
-document.getElementById('1').addEventListener('click', function(){
-	//Juan's func(document.getElementById('1').lastChild)
-	console.log('clicked');
-	playVid(document.getElementById('1').lastChild);
-});
-document.getElementById('2').addEventListener('click', function(){
-	//Juan's func(document.getElementById('2').lastChild)
-	console.log('clicked');
-	playVid(document.getElementById('2').lastChild);
-});
-document.getElementById('3').addEventListener('click', function(){
-	//Juan's func(document.getElementById('3').lastChild)
-	console.log('clicked');
-	playVid(document.getElementById('3').lastChild);
-});
-document.getElementById('4').addEventListener('click', function(){
-	//Juan's func(document.getElementById('4').lastChild)
-	console.log('clicked');
-	playVid(document.getElementById('4').lastChild);
-});
-document.getElementById('5').addEventListener('click', function(){
-	//Juan's func(document.getElementById('5').lastChild)
-	console.log('clicked');
-	playVid(document.getElementById('5').lastChild);
-});
+function selectVid() {
+	//Juan
+	playVideo(ID);
 }
 
 function playVideo(ID) {
